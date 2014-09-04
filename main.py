0e08b30fc9d0f4f0c3c01b9fe052ca9333755a9d
tswinnie <tyrone.swinnie@gmail.com>
2014-09-04 17:41:53 -0400

    created array
    I created an array that I will use to store the information that I will
    collect from the user

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-# print "Hello Tyrone"
+'''
 
+Tyrone Swinnie
+Design for Web Standards
+Madlib Assignment
+9/4/14
 '''
-The first thing I want to do is create questions
-so I can collect information for my madlib
-'''
+
+# i will create variables that will hold that data from user
+
 
 adj1 = raw_input('Tell me an adjective, then press enter')
 num1 = raw_input('Give me a number between 1 and 10 and press enter.')
@@ -14,4 +18,10 @@ num3 = raw_input('Give me a number between 1 and 5 and press enter.')
 
 
 
-# print adj1
+'''
+Now I need to store the variables that have
+the data from the user. I will use an array
+
+'''
+myArray = [adj1, num1, noun1, num2, ver1, num3]
+# print myArray
