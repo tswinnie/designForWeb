e5a538788a2cbd0c6053c5edd63ef8c22729c9c7
tswinnie <tyrone.swinnie@gmail.com>
2014-09-04 17:32:02 -0400

    created questions
    I created questions that I will use to get information for my mad lib
    game

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print "Hello Tyrone"
+# print "Hello Tyrone"
+
+'''
+The first thing I want to do is create questions
+so I can collect information for my madlib
+'''
+
+adj1 = raw_input('Tell me an adjective, then press enter')
+num1 = raw_input('Give me a number between 1 and 10 and press enter.')
+noun1 = raw_input('Tell me an noun, then press enter')
+num2 = raw_input('Give me a number between 1 and 8 and press enter.')
+ver1 = raw_input('Tell me an verb, then press enter')
+num3 = raw_input('Give me a number between 1 and 5 and press enter.')
+
+
+
+# print adj1
